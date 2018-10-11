bd3df25ce37812dacd3f1f2281687523a7a8e0a9
Arivaldo do Nascimento Silva <arivaldon.silva@gmail.com>
2018-10-11 16:24:22 -0300

    Terminando as funcionalidades do carrinho

--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -34,9 +34,55 @@ export class CartService{
         if(position == -1){
             cart.items.push({quantidade: 1, produto: produto });
         }
-        
-        
+        else{
+            cart.items[position].quantidade += 1;
+        }
         this.storage.setCart(cart);
         return cart;
+    }
+
+    // remove um produto do carrinho
+    removeProduto(produto: ProdutoDTO): Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1){
+            cart.items.splice(position, 1);
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    // aumenta em uma unidade a quantidade de item do carrinho
+    increaseQuantity(produto: ProdutoDTO):Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1){
+            cart.items[position].quantidade++;
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    // reduz em uma unidade a quantidade de item do carrinho
+    decreaseQuantity(produto: ProdutoDTO):Cart{
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1){
+            cart.items[position].quantidade--;
+            if(cart.items[position].quantidade < 1){
+                cart = this.removeProduto(produto);
+            }
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    total(): number{
+        let cart = this.storage.getCart();
+        let sum = 0;
+        for(var i=0; i < cart.items.length; i++){
+            sum += cart.items[i].quantidade * cart.items[i].produto.preco;
+        }
+        return sum;
     }
 }

b3674a5318789dfecc5a409a0e38c7612b96850c
Arivaldo do Nascimento Silva <arivaldon.silva@gmail.com>
2018-10-13 15:11:31 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
 
     this.pedido = navParams.get('pedido');
   }
@@ -47,6 +49,25 @@ export class OrderConfirmationPage {
 
   total(){
     return this.cartService.total();
+  }
+
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        //if(response.status == 201){
+          console.log('Pedido criado com sucesso: '+response.headers.get('location'));
+          this.cartService.createOrClearCart();
+       // }
+      }
+      , error => {
+        if(error.status == 403){
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
